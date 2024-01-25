e59a07021578836e4e5dabaddf22df8de70575d3
deoresulakshana <deoresulakshana@gmail.com>
2024-01-25 16:23:20 +0530

    Add project files.

---
+++ DelegatesApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkingWithDelegatesLibrary;
+namespace DelegatesApp
+{
+    internal class Program
+    {
+        //declaration of the event
+        static event DelegateForLeaveApprovalProcess ApplyForLeave;
+        static void Main(string[] args)
+        {
+            Employee empobj = new Employee() { Empid=1,EmpName="Ajay",TotalLeaves=10};
+
+            DelegateForLeaveApprovalProcess delobj = new DelegateForLeaveApprovalProcess(empobj.LeaveApproval);//reference of our method
+            ApplyForLeave += delobj;
+            Console.WriteLine("Do u want to apply for leave");
+            bool userchoice =Convert.ToBoolean(Console.ReadLine());
+            if (userchoice==true)
+            {
+                Console.WriteLine("For how many days u want leave");
+                int leave = Convert.ToInt32(Console.ReadLine());
+                bool ans = ApplyForLeave(leave);
+                Console.WriteLine("Leave Approved= " + ans);
+                Console.WriteLine($"Now the total leaves remaining ={empobj.TotalLeaves}");
+            }
+            else
+            {
+                Console.WriteLine("Ok...coming out of the leave application process");
+            }
+
+
+
+           
+            Console.Read();
+        }
+    }
+}
---
+++ WorkingWithDelegatesLibrary/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDelegatesLibrary
+{
+    public delegate bool DelegateForLeaveApprovalProcess(int noOfDays);
+    public class Employee
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+        public int TotalLeaves { get; set; }
+
+        public bool LeaveApproval(int noOfDays)
+        {
+            bool leaveApprovalStatus = false;
+        if (TotalLeaves > noOfDays)
+            {
+                Console.WriteLine("You can take a leave");
+                TotalLeaves = TotalLeaves - noOfDays;
+                leaveApprovalStatus = true;
+            }
+            else
+            {
+                Console.WriteLine("You can't take a leave... Leave not approved");
+
+            }
+        return leaveApprovalStatus;
+
+
+
+        }
+
+    }
+}
