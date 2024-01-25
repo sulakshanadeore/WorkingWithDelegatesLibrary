7a777a54f7fa49f0e277ca123a0879a211ac24ab
deoresulakshana <deoresulakshana@gmail.com>
2024-01-25 16:56:30 +0530

    multicast demo with concat name and string length

--- DelegatesApp/Program.cs
+++ DelegatesApp/Program.cs
@@ -12,13 +12,28 @@ namespace DelegatesApp
         static event DelegateForLeaveApprovalProcess ApplyForLeave;
         static void Main(string[] args)
         {
-            Employee empobj = new Employee() { Empid=1,EmpName="Ajay",TotalLeaves=10};
+           // UnicastDelegateDemo();
+
+            Employee empobj=new Employee();
+            EmpFullNameAndLength delobj1 = new EmpFullNameAndLength(empobj.CreateFullName);
+            EmpFullNameAndLength delobj2 = new EmpFullNameAndLength(empobj.CalculateLengthOfName);
+            EmpFullNameAndLength delobjCombined=(EmpFullNameAndLength)MulticastDelegate.Combine(delobj1, delobj2);
+            
+            delobjCombined.Invoke("Amitabh ", "Bacchan");
+            Console.WriteLine(empobj.EmpName);
+
+            Console.Read();
+        }
+
+        private static void UnicastDelegateDemo()
+        {
+            Employee empobj = new Employee() { Empid = 1, EmpName = "Ajay", TotalLeaves = 10 };
 
             DelegateForLeaveApprovalProcess delobj = new DelegateForLeaveApprovalProcess(empobj.LeaveApproval);//reference of our method
             ApplyForLeave += delobj;
             Console.WriteLine("Do u want to apply for leave");
-            bool userchoice =Convert.ToBoolean(Console.ReadLine());
-            if (userchoice==true)
+            bool userchoice = Convert.ToBoolean(Console.ReadLine());
+            if (userchoice == true)
             {
                 Console.WriteLine("For how many days u want leave");
                 int leave = Convert.ToInt32(Console.ReadLine());
@@ -30,11 +45,6 @@ namespace DelegatesApp
             {
                 Console.WriteLine("Ok...coming out of the leave application process");
             }
-
-
-
-           
-            Console.Read();
         }
     }
 }
--- WorkingWithDelegatesLibrary/Employee.cs
+++ WorkingWithDelegatesLibrary/Employee.cs
@@ -7,13 +7,29 @@ using System.Threading.Tasks;
 namespace WorkingWithDelegatesLibrary
 {
     public delegate bool DelegateForLeaveApprovalProcess(int noOfDays);
+
+    public delegate void EmpFullNameAndLength(string Fname, string Lname);
     public class Employee
     {
         public int Empid { get; set; }
         public string EmpName { get; set; }
+
+       
         public int TotalLeaves { get; set; }
 
-        public bool LeaveApproval(int noOfDays)
+
+        public void CreateFullName(string fname, string lname) {
+            EmpName= string.Concat(fname, lname);
+            
+        }
+
+        public void CalculateLengthOfName(string fname, string lname)
+        {
+          int namelength=string.Concat(fname, lname).Length;
+            Console.WriteLine(namelength);
+        }
+
+            public bool LeaveApproval(int noOfDays)
         {
             bool leaveApprovalStatus = false;
         if (TotalLeaves > noOfDays)
